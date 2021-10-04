657b7f8b6c5dab78b139cb10c65280c0096e3d3a
unknown <a.petkovpetkov@gmail.com>
2021-10-04 13:24:25 +0300

    Fixing Twitter connection issue

--- microservices-demo/twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
+++ microservices-demo/twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/config/TwitterToKafkaServiceConfigData.java
@@ -1,29 +1,15 @@
 package com.microservices.demo.twitter.to.kafka.service.config;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.List;
 
+@Data
 @Configuration
 @ConfigurationProperties(prefix = "twitter-to-kafka-service")
 public class TwitterToKafkaServiceConfigData {
-    private List<String> twitterKeyWords;
+    private List<String> twitterKeywords;
     private String welcomeMessage;
-
-    public List<String> getTwitterKeyWords() {
-        return twitterKeyWords;
-    }
-
-    public void setTwitterKeyWords(List<String> twitterKeyWords) {
-        this.twitterKeyWords = twitterKeyWords;
-    }
-
-    public String getWelcomeMessage() {
-        return welcomeMessage;
-    }
-
-    public void setWelcomeMessage(String welcomeMessage) {
-        this.welcomeMessage = welcomeMessage;
-    }
 }
