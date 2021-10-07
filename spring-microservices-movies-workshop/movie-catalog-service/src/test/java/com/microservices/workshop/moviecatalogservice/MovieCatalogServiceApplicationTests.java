13fc7b82705ddf9d4abb51a2e2d11d0c4eb1eae9
unknown <a.petkovpetkov@gmail.com>
2021-10-07 19:05:55 +0300

    Adding spring initializr projects

---
+++ spring-microservices-movies-workshop/movie-catalog-service/src/test/java/com/microservices/workshop/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.workshop.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
