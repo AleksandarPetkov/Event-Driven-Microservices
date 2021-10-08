d64e19d6c705a60107a58d569582acaec0c92082
unknown <a.petkovpetkov@gmail.com>
2021-10-08 16:22:34 +0300

    using RestTemplate and WebClient to make API call

--- spring-microservices-movies-workshop/movie-catalog-service/src/main/java/com/microservices/workshop/moviecatalogservice/web/MovieCatalogController.java
+++ spring-microservices-movies-workshop/movie-catalog-service/src/main/java/com/microservices/workshop/moviecatalogservice/web/MovieCatalogController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,27 +17,42 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogController {
 
+    private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
 
+    public MovieCatalogController(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+        this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
+    }
 
     @RequestMapping("/{userId}")
-    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         /**
          Steps:
          1. Get all rated movie Ids
          2. For each Id call movie info service and get det the details
          3. Put them all together
          **/
-        RestTemplate restTemplate = new RestTemplate();
-
         List<Rating> ratings = Arrays.asList(
-          new Rating("movie1",2), new Rating("movie2",5), new Rating("movie2",5)
+                new Rating("movie1", 2), new Rating("movie2", 5), new Rating("movie2", 5)
         );
 
         return ratings
                 .stream()
                 .map(r -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8001/movie/"+ r.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(), "Descriptions", r.getRating());
-        }).collect(Collectors.toList());
+                    //Calling other Microservice by RestTemplate
+//            Movie movie = restTemplate.
+//            getForObject("http://localhost:8001/movie/"+ r.getMovieId(), Movie.class);
+
+                    //Calling other Microservice by WebClient
+                    Movie movie = webClientBuilder.build()
+                            .get() //HTTP Method (GET, Post...)
+                            .uri("http://localhost:8001/movie/" + r.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class) //When data is come back convert it to Movie.class
+                            .block();
+
+                    return new CatalogItem(movie.getName(), "Descriptions", r.getRating());
+                }).collect(Collectors.toList());
     }
 }
