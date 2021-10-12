fbc9471c94e80b759f568c471e8adf67c863a752
unknown <a.petkovpetkov@gmail.com>
2021-10-12 10:56:56 +0300

    Calling extarnal API

--- spring-microservices-movies-workshop/movie-info-service/src/main/java/com/microservices/workshop/movieinfoservice/MovieInfoServiceApplication.java
+++ spring-microservices-movies-workshop/movie-info-service/src/main/java/com/microservices/workshop/movieinfoservice/MovieInfoServiceApplication.java
@@ -2,11 +2,20 @@ package com.microservices.workshop.movieinfoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MovieInfoServiceApplication {
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		// Do any additional configuration here
+		return builder.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieInfoServiceApplication.class, args);
