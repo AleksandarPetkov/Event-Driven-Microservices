89faab923f8f0f72f613a469527c5e4367d5ebbe
unknown <a.petkovpetkov@gmail.com>
2021-10-29 16:30:27 +0300

    Creating new Bank Account microservices project

---
+++ bank-system/bank.system.accounts/src/main/java/com/banksystemmicroservicesaccounts/Bank/System/Accounts/BankSystemAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.banksystemmicroservicesaccounts.Bank.System.Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystemAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSystemAccountsApplication.class, args);
+	}
+
+}
---
+++ bank-system/bank.system.accounts/src/test/java/com/banksystemmicroservicesaccounts/Bank/System/Accounts/BankSystemAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksystemmicroservicesaccounts.Bank.System.Accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
